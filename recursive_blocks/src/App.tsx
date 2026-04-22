095a846c35e71ab097af038bbaa6b265c7bd7476
hinyan17 <hinyan17@gmail.com>
2026-04-22 12:48:01 -0400

    update

--- recursive_blocks/src/App.tsx
+++ recursive_blocks/src/App.tsx
@@ -1,21 +1,35 @@
+import { useEffect, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { BlockEditor } from "./BlockEditor";
 import { BlockEditorProvider } from "./BlockEditorContext";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Documentation from "./Documentation";
+import DocsPage from "./DocsPage";
+
+function getCurrentView(): "editor" | "docs" {
+  return window.location.hash === "#docs" ? "docs" : "editor";
+}
 
 export default function App() {
+  const [view, setView] = useState<"editor" | "docs">(getCurrentView);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setView(getCurrentView());
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
+  if (view === "docs") {
+    return <DocsPage />;
+  }
+
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="app-container">
         <BlockEditorProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<BlockEditor />} />
-              <Route path="/docs" element={<Documentation />} />
-            </Routes>
-          </Router>
+          <BlockEditor />
         </BlockEditorProvider>
       </div>
     </DndProvider>
